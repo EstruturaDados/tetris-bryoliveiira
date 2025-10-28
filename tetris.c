b68f66a8d91e69231aec7285657d2759fb6d2975
Bryanna Oliveira <bryanna.suport@gmail.com>
2025-10-27 23:53:47 -0300

    Update tetris.c
    feat: implementado ações 'Reservar Peça' e 'Usar Peça Reservada', integrando a nova Pilha de Reserva ao fluxo da Fila.

--- tetris.c
+++ tetris.c
@@ -3,55 +3,76 @@
 #include <time.h>
 
 #define TAM_FILA 5
+#define TAM_PILHA 3
 
 typedef struct {
-    char nome; 
-    int id;   
+    char nome;
+    int id;
 } Peca;
 
 typedef struct {
-    Peca pecas[TAM_FILA]; 
-    int inicio;           
-    int fim;              
-    int contador;         
+    Peca pecas[TAM_FILA];
+    int inicio;
+    int fim;
+    int contador;
 } Fila;
 
-Fila filaDePecas;      
-int proximoId = 0;      
+typedef struct {
+    Peca pecas[TAM_PILHA];
+    int topo;
+} Pilha;
+
+Fila filaDePecas;
+Pilha pilhaDeReserva;
+int proximoId = 0;
 
 Peca gerarPeca(void);
 void inicializarFila(void);
+void inicializarPilha(void);
+
 int filaEstaCheia(void);
 int filaEstaVazia(void);
-void enqueue(void);
-void dequeue(void);
+int pilhaEstaCheia(void);
+int pilhaEstaVazia(void);
+
+Peca helper_dequeue(void);
+void helper_enqueue(Peca p);
+
+void acao_jogarPeca(void);
+void acao_reservarPeca(void);
+void acao_usarPecaReservada(void);
+
+void exibirEstado(void);
 void exibirFila(void);
+void exibirPilha(void);
 void exibirMenu(void);
 
 int main() {
     srand(time(NULL));
-
     int opcao;
 
     inicializarFila();
+    inicializarPilha();
 
     do {
-        exibirFila();
+        exibirEstado();
         exibirMenu();
 
         if (scanf("%d", &opcao) != 1) {
             while (getchar() != '\n');
-            opcao = -1; 
+            opcao = -1;
         }
-
-        printf("\n"); 
+        printf("\n");
 
         switch (opcao) {
             case 1:
-                dequeue(); 
+                acao_jogarPeca();
                 break;
             case 2:
-                enqueue(); 
+                acao_reservarPeca();
+                break;
+            case 3:
+                acao_usarPecaReservada();
                 break;
             case 0:
                 printf("Saindo do Tetris Stack...\n");
@@ -61,37 +82,33 @@ int main() {
                 break;
         }
 
-    } while (opcao != 0); 
+    } while (opcao != 0);
 
     return 0;
 }
 
 Peca gerarPeca() {
     Peca novaPeca;
-    char tiposDePeca[] = {'I', 'O', 'T', 'L'}; 
+    char tiposDePeca[] = {'I', 'O', 'T', 'L', 'S', 'Z', 'J'};
     
-    novaPeca.nome = tiposDePeca[rand() % 4];
-    
-    novaPeca.id = proximoId;
-    proximoId++;
-    
+    novaPeca.nome = tiposDePeca[rand() % 7];
+    novaPeca.id = proximoId++;
     return novaPeca;
 }
 
 void inicializarFila() {
     filaDePecas.inicio = 0;
-    filaDePecas.fim = -1; 
+    filaDePecas.fim = -1;
     filaDePecas.contador = 0;
 
     printf("Inicializando fila com %d peças...\n", TAM_FILA);
-
     for (int i = 0; i < TAM_FILA; i++) {
-        Peca p = gerarPeca();
-        
-        filaDePecas.fim = (filaDePecas.fim + 1) % TAM_FILA;
-        filaDePecas.pecas[filaDePecas.fim] = p;
-        filaDePecas.contador++;
+        helper_enqueue(gerarPeca());
     }
+}
+
+void inicializarPilha() {
+    pilhaDeReserva.topo = -1;
 }
 
 int filaEstaCheia() {
@@ -102,62 +119,111 @@ int filaEstaVazia() {
     return (filaDePecas.contador == 0);
 }
 
-void enqueue() {
-    if (filaEstaCheia()) {
-        printf("Ação: FALHA. A fila de próximas peças esta cheia!\n");
-        printf("É preciso jogar uma peça (op 1) antes de inserir.\n");
-    } else {
-        Peca novaPeca = gerarPeca();
-        
-        filaDePecas.fim = (filaDePecas.fim + 1) % TAM_FILA;
-        
-        filaDePecas.pecas[filaDePecas.fim] = novaPeca;
-        filaDePecas.contador++;
-        
-        printf("Ação: Nova peça [%c %d] inserida no final da fila.\n", novaPeca.nome, novaPeca.id);
-    }
+int pilhaEstaCheia() {
+    return (pilhaDeReserva.topo == TAM_PILHA - 1);
 }
 
-void dequeue() {
+int pilhaEstaVazia() {
+    return (pilhaDeReserva.topo == -1);
+}
+
+Peca helper_dequeue(void) {
+    Peca pecaRemovida = filaDePecas.pecas[filaDePecas.inicio];
+    filaDePecas.inicio = (filaDePecas.inicio + 1) % TAM_FILA;
+    filaDePecas.contador--;
+    return pecaRemovida;
+}
+
+void helper_enqueue(Peca p) {
+    if (filaEstaCheia()) {
+        printf("ERRO INTERNO: Tentativa de enfileirar em fila cheia.\n");
+        return;
+    }
+    filaDePecas.fim = (filaDePecas.fim + 1) % TAM_FILA;
+    filaDePecas.pecas[filaDePecas.fim] = p;
+    filaDePecas.contador++;
+}
+
+void acao_jogarPeca(void) {
     if (filaEstaVazia()) {
         printf("Ação: FALHA. A fila de peças esta vazia!\n");
-        printf("é preciso inserir uma nova peça (op 2).\n");
-    } else {
-        Peca pecaJogada = filaDePecas.pecas[filaDePecas.inicio];
-        
-        filaDePecas.inicio = (filaDePecas.inicio + 1) % TAM_FILA;
-        
-        filaDePecas.contador--;
-        
-        printf("Ação: Peça [%c %d] jogada (removida da frente).\n", pecaJogada.nome, pecaJogada.id);
+        return;
     }
+
+    Peca pecaJogada = helper_dequeue();
+    printf("Ação: Peça [%c %d] jogada (removida da frente).\n", pecaJogada.nome, pecaJogada.id);
+    
+    helper_enqueue(gerarPeca());
+}
+
+void acao_reservarPeca(void) {
+    if (pilhaEstaCheia()) {
+        printf("Ação: FALHA. A pilha de reserva esta cheia! (Max: %d)\n", TAM_PILHA);
+        return;
+    }
+    if (filaEstaVazia()) {
+        printf("Ação: FALHA. A fila de peças esta vazia!\n");
+        return;
+    }
+
+    Peca pecaReservada = helper_dequeue();
+    pilhaDeReserva.pecas[++pilhaDeReserva.topo] = pecaReservada;
+    
+    printf("Ação: Peça [%c %d] movida da fila para a reserva.\n", pecaReservada.nome, pecaReservada.id);
+
+    helper_enqueue(gerarPeca());
+}
+
+void acao_usarPecaReservada(void) {
+    if (pilhaEstaVazia()) {
+        printf("Ação: FALHA. A pilha de reserva esta vazia!\n");
+        return;
+    }
+
+    Peca pecaUsada = pilhaDeReserva.pecas[pilhaDeReserva.topo--];
+    printf("Ação: Peça reservada [%c %d] usada (removida da pilha).\n", pecaUsada.nome, pecaUsada.id);
+
+    helper_enqueue(gerarPeca());
+}
+
+void exibirEstado() {
+    printf("\n--- Estado Atual do Jogo ---\n");
+    exibirFila();
+    exibirPilha();
+    printf("----------------------------\n");
 }
 
 void exibirFila() {
-    printf("\n--- Estado Atual da Fila ---\n");
-    
+    printf("Fila de peças: ");
     if (filaEstaVazia()) {
-        printf("Fila de peças: [ VAZIA ]\n");
+        printf("[ VAZIA ]\n");
     } else {
-        printf("Fila de peças: ");
-        
         int i = filaDePecas.inicio;
-        
         for (int c = 0; c < filaDePecas.contador; c++) {
             printf("[%c %d] ", filaDePecas.pecas[i].nome, filaDePecas.pecas[i].id);
-            
             i = (i + 1) % TAM_FILA;
         }
         printf("\n");
     }
-    printf("-----------------------------\n");
+}
+
+void exibirPilha() {
+    printf("Pilha de reserva (Topo -> Base): ");
+    if (pilhaEstaVazia()) {
+        printf("[ VAZIA ]\n");
+    } else {
+        for (int i = pilhaDeReserva.topo; i >= 0; i--) {
+            printf("[%c %d] ", pilhaDeReserva.pecas[i].nome, pilhaDeReserva.pecas[i].id);
+        }
+        printf("\n");
+    }
 }
 
 void exibirMenu() {
     printf("\nOpções de ação:\n");
-    printf(" 1 - Jogar peça (dequeue)\n");
-    printf(" 2 - Inserir nova peça (enqueue)\n");
+    printf(" 1 - Jogar peça (da fila)\n");
+    printf(" 2 - Reservar peça (fila -> pilha)\n");
+    printf(" 3 - Usar peça reservada (da pilha)\n");
     printf(" 0 - Sair\n");
     printf("Escolha sua ação: ");
-
 }
